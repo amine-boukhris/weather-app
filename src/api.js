4e9e9c4f886673934e1a89a16d959ba06a243f2e
amine <boukhrisamine210@gmail.com>
2023-10-04 15:42:48 +0100

    back to coding

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: "GET",
+	headers: {
+		"X-RapidAPI-Key": "a2c5b7daecmshc7941d1f1b66fd8p125d4ajsnee14e453c125",
+		"X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+	},
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
